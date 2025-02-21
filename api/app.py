a27ac9fd54c99c35c9dcdca38527073606338d62
maltestaab <115007929+maltestaab@users.noreply.github.com>
2025-02-21 15:38:31 +0100

    structure

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
 
 app = FastAPI()
 
@@ -9,8 +10,20 @@ class InputPayload(BaseModel):
     petal_length: float
     petal_width: float
 
+
+class OutputPayload(BaseModel):
+    flower_type: str
+
+loaded_model = pickle.load(open(model.pkl, 'rb'))
+
+
 @app.post("/echo/")
 
 def send_back_input(input: InputPayload):
+
+    input_array = np.array([[input.sepal_length, input.sepal_width, input.petal_length, input.petal_width]])
+
+    prediction = loaded_model.predict(input_array)
+
     return {"message": f"Input: {input.sepal_length} Sepal Length. {input.sepal_width} \
-            Sepal Width. {input.petal_length} Petal Length. {input.petal_width} Petal Width."}
+            Sepal Width. {input.petal_length} Petal Length. {input.petal_width} Pe
